2fae852f50d69ae6bce2445ce49fc2b6f2fb1b1a
WhyDojo <grinvaurdourin@gmail.com>
2026-03-08 20:09:01 +0300

    add new section notice and about-hero and styles + a little script for notice

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const noticeEl = document.querySelector(".notice");
+
+if (noticeEl) {
+  const noticeCloseEl = noticeEl.querySelector(".notice__close");
+
+  noticeCloseEl.addEventListener("click", () => {
+    noticeEl.classList.add("notice--hidden");
+  });
+}
